f55082b9eaea2034e818f3ffb5324c0b1f906ed4
Anil Kumar Kothapalli <anilkumar1988@gmail.com>
2020-05-08 20:38:50 +0200

    sorting data based on name and office

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,13 +6,31 @@ const loadContacts = (contacts) => ({
   contacts,
 });
 
+const compare = (firstContact, secondContact) => {
+  const firstName = firstContact.name.toLowerCase();
+  const secondName = secondContact.name.toLowerCase();
+
+  if (firstName > secondName) {
+    return 1;
+  }
+
+  if (firstName < secondName) {
+    return -1;
+  }
+
+  if (firstName === secondName) {
+    return firstContact.office > secondContact.office ? 1 : -1;
+  }
+};
+
 export const loadContactsAsync = () => {
   return (dispatch, getState) => {
     return fetch(config.API_URL)
       .then((res) => res.json())
       .then(
         (contacts) => {
-          dispatch(loadContacts(contacts));
+          const sortedContacts = contacts.sort(compare);
+          dispatch(loadContacts(sortedContacts));
         },
         (error) => {
           console.log('Error loading contacts');
