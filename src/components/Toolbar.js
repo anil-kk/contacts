c2d91fe5d74449cf25e0872455e8b13aa1b630c4
Anil Kumar Kothapalli <anilkumar1988@gmail.com>
2020-05-09 16:10:20 +0200

    adjustments for mobile views

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Pagination, Menu, Icon } from 'semantic-ui-react';
+import { Container, Pagination, Menu, Icon, Grid } from 'semantic-ui-react';
 
 const Toolbar = ({
   handleMenuItemClick,
@@ -9,31 +9,43 @@ const Toolbar = ({
   totalPages,
 }) => {
   return (
-    <Container>
-      <Menu compact icon='labeled' floated="right">
-        <Menu.Item
-          name='grid'
-          active={activeItem === 'grid'}
-          onClick={handleMenuItemClick}
-        >
-          <Icon name='grid layout' />
-          Grid
-        </Menu.Item>
-        <Menu.Item
-          name='list'
-          active={activeItem === 'list'}
-          onClick={handleMenuItemClick}
-        >
-          <Icon name='list' />
-          List
-        </Menu.Item>
-      </Menu>
-      <Pagination
-        activePage={currentPage}
-        onPageChange={handlePaginationChange}
-        totalPages={totalPages}
-      />
-    </Container>
+    <Grid columns={2} stackable>
+      <Grid.Row>
+        <Grid.Column>
+          <Container>
+            <Menu compact>
+              <Menu.Item
+                name='grid'
+                active={activeItem === 'grid'}
+                onClick={handleMenuItemClick}
+              >
+                <Icon name='grid layout' />
+              </Menu.Item>
+              <Menu.Item
+                name='list'
+                active={activeItem === 'list'}
+                onClick={handleMenuItemClick}
+              >
+                <Icon name='list' />
+              </Menu.Item>
+            </Menu>
+          </Container>
+        </Grid.Column>
+        <Grid.Column>
+          <Container>
+            <Pagination
+              activePage={currentPage}
+              onPageChange={handlePaginationChange}
+              totalPages={totalPages}
+              ellipsisItem={null}
+              siblingRange={0}
+              firstItem={null}
+              lastItem={null}
+            />
+          </Container>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 };
 
